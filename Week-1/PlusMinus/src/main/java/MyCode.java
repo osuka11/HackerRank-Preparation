99da6a0919ed741cf713fd0d8b29edf8534484a9
Osuka <oscarincon21@hotmail.com>
2022-12-18 20:20:10 -0600

    PlusMinus in Java

--- Week-1/PlusMinus/src/main/java/MyCode.java
+++ Week-1/PlusMinus/src/main/java/MyCode.java
@@ -1,6 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyCode {
 
-    public static void main(String[] args){
-        
+    public static void plusMinus(List<Integer> arr) {
+        float positives = ((float) arr.stream().filter(v -> v >= 0 && v <= 100).count());
+        float negatives = ((float) arr.stream().filter(v -> v <= 0 && v >= -100).count());
+        float zeros = ((float) arr.stream().filter(v -> v == 0).count());
+
+
+        System.out.printf("%.6f",positives/arr.size());
+        System.out.println();
+        System.out.printf("%.6f",negatives/arr.size());
+        System.out.println();
+        System.out.printf("%.6f",zeros/arr.size());
     }
 }
