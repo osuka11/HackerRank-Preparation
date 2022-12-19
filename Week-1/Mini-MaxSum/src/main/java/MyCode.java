aa277bf0d6314c04ba4e133ce698f3e126a7e342
Osuka <oscarincon21@hotmail.com>
2022-12-18 20:46:25 -0600

    Min Max Sum in Java

--- Week-1/Mini-MaxSum/src/main/java/MyCode.java
+++ Week-1/Mini-MaxSum/src/main/java/MyCode.java
@@ -1,2 +1,17 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MyCode {
+    public static void miniMaxSum(List<Integer> arr) {
+        Collections.sort(arr);
+        List<Long> arrLong = arr.stream().map(Integer::longValue).collect(Collectors.toList());
+        // arrLong.forEach(System.out::println);
+        Long maxSum = (arrLong.stream().reduce(1L, (a,b) -> a+b )) - arrLong.get(0);
+        Long minSum = (arrLong.stream().reduce(1L, (a,b) -> a+b )) - arrLong.get(arrLong.size()-1);
+        System.out.println(maxSum + " " + minSum);
+    }
+
 }
+
--- Week-1/Mini-MaxSum/src/main/kotlin/MyCode.kt
+++ Week-1/Mini-MaxSum/src/main/kotlin/MyCode.kt
@@ -1,22 +1,23 @@
 
-fun plusMinus(arr: Array<Int>): Unit {
-
-    val zeros: Float = arr.filter { z -> z == 0 }.size.toFloat()
-    val negatives: Float = arr.filter { z -> z <= -1 && z >= -1000  }.size.toFloat()
-    val positives: Float = arr.filter { z -> z >= 1 && z <= 1000 }.size.toFloat()
-
-    println(String.format("%.6f", positives/arr.size))
-    println(String.format("%.6f", negatives/arr.size))
-    println(String.format("%.6f", zeros/arr.size))
-
-
+fun miniMaxSum(arr: Array<Int>): Unit {
+    var minSum : Long = 0L
+    var maxSum : Long = 0L
+    arr.sort()
+    if (arr.first() == arr.last()){
+        println("${arr.sum() - arr[0]} ${arr.sum() - arr[0]}")
+    }else{
+        for(i in arr){
+            if( i > arr.first()) maxSum += i
+            if( i<arr.last()) minSum += i
+        }
+        println("$minSum $maxSum")
+    }
 
 }
 
 fun main(args: Array<String>) {
-    val n = readLine()!!.trim().toInt()
 
     val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
 
-    plusMinus(arr)
+    miniMaxSum(arr)
 }
